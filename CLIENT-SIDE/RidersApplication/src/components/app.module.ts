fcdeb1ddc5ba9e9f8f2361593bed922d2e809783
vagbhi <vagbhi@gmail.com>
2021-04-14 11:51:54 +0530

    login

--- CLIENT-SIDE/RidersApplication/src/components/app.module.ts
+++ CLIENT-SIDE/RidersApplication/src/components/app.module.ts
@@ -1,17 +1,25 @@
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RootComponent } from './root/root.component';
+import { RootComponent } from './root/root.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
-  declarations: [RootComponent],
+  declarations: [RootComponent, LoginComponent],
   imports: [
     CommonModule,
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap:[
-    RootComponent
+    RootComponent,
+    
   ]
 })
 export default class AppModule { }
