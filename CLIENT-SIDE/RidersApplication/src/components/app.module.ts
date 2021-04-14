4cc895aa4e87048b8ee0afec9e31be5796281bd5
vagbhi <vagbhi@gmail.com>
2021-04-14 14:35:00 +0530

    home and registration

--- CLIENT-SIDE/RidersApplication/src/components/app.module.ts
+++ CLIENT-SIDE/RidersApplication/src/components/app.module.ts
@@ -1,3 +1,5 @@
+import { RegistrationComponent } from './registration/registration.component';
+import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,21 +7,28 @@ import { RootComponent } from './root/root.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes:Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomepageComponent },
+  {path: 'registration', component: RegistrationComponent}
+];
 
 @NgModule({
-  declarations: [RootComponent, LoginComponent],
+  declarations: [RootComponent, LoginComponent, HomepageComponent, RegistrationComponent],
   imports: [
     CommonModule,
     BrowserModule,
     NgbModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap:[
     RootComponent,
-    
+       
   ]
 })
 export default class AppModule { }
