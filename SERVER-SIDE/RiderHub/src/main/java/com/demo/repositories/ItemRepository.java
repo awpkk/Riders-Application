79f1d7666c3c0fbfe6d93f197f9b4992b966771e
vagbhi <vagbhi@gmail.com>
2021-04-20 10:12:56 +0530

    added css

--- SERVER-SIDE/RiderHub/src/main/java/com/demo/repositories/ItemRepository.java
+++ SERVER-SIDE/RiderHub/src/main/java/com/demo/repositories/ItemRepository.java
@@ -1,11 +1,10 @@
 package com.demo.repositories;
 
-
 import org.springframework.data.repository.CrudRepository;
 
 import com.demo.entities.Item;
 
-
 public interface ItemRepository extends CrudRepository<Item,Integer>{
 
 }
+		
