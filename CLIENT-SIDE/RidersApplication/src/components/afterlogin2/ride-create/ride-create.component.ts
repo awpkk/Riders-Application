1457ccccaa97c3114e6a483b3cc821459d9249c6
vagbhi <vagbhi@gmail.com>
2021-04-26 10:49:34 +0530

    validations

--- CLIENT-SIDE/RidersApplication/src/components/afterlogin2/ride-create/ride-create.component.ts
+++ CLIENT-SIDE/RidersApplication/src/components/afterlogin2/ride-create/ride-create.component.ts
@@ -19,14 +19,12 @@ export class RideCreateComponent implements OnInit {
   maxday: number;
   minDate: { year: number; month: number; day: number; };
   // endDate: { year: number; month: number; day: number; };
-  tripEndDate: { year: number; month: number; day: number;  }
+  tripEndDate: { year: number; month: number; day: number; }
   rider: any;
   creatorName1: String;
-  
-
+  endDate: { year: number; month: number; day: number; };
   //private url: string = "http://localhost:8787/rides/create"
   //ridersList: any = {};
-
   open(content) {
     this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
@@ -34,7 +32,6 @@ export class RideCreateComponent implements OnInit {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
-
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
@@ -44,7 +41,6 @@ export class RideCreateComponent implements OnInit {
       return `with: ${reason}`;
     }
   }
-
   constructor(private router: Router, private modalService: NgbModal, private riderService: RiderService, private rideService: RideService, private activatedroute: ActivatedRoute) {
     this.activatedroute.params.subscribe(data => {
       this.email = data.email;
@@ -55,54 +51,44 @@ export class RideCreateComponent implements OnInit {
   public currentMonth: number = this.today.getMonth();
   public currentDay: number = this.today.getDate();
   //public minDate: Object = new Date(this.currentYear, this.currentMonth, this.currentDay);
- 
   ngOnInit(): void {
     this.rideForm = new FormGroup({
       creatorName: new FormControl(this.creatorName1),
-      title: new FormControl("", Validators.required),
-      description: new FormControl("", Validators.required),
-      source: new FormControl("", Validators.required),
-      destination: new FormControl("", Validators.required),
-      maxRiders: new FormControl("", Validators.required),
+      title: new FormControl("", [Validators.required, Validators.pattern("^(?! )[a-zA-Z0-9(?! )]*$")]),
+      description: new FormControl("", [Validators.required, Validators.pattern("^(?! )[a-zA-Z0-9(?! )]*$")]),
+      source: new FormControl("", [Validators.required, Validators.pattern("^(?! )[a-zA-Z0-9(?! )]*$")]),
+      destination: new FormControl("", [Validators.required, Validators.pattern("^(?! )[a-zA-Z0-9(?! )]*$")]),
+      maxRiders: new FormControl("", [Validators.required, Validators.min(2)]),
       startdate: new FormControl("", Validators.required),
       enddate: new FormControl("", Validators.required)
     });
-
     this.findRiderByEmail();
-
-
-    this.maxday=this.rideForm.value.startdate.day;
-    this.minDate={"year":this.currentYear,"month":this.currentMonth+1,"day":this.currentDay};
-    this.minDate={"year":this.currentYear,"month":this.currentMonth+1,"day":this.currentDay};
+    this.maxday = this.rideForm.value.startdate.day;
+    this.minDate = { "year": this.currentYear, "month": this.currentMonth + 1, "day": this.currentDay };
+    this.minDate = { "year": this.currentYear, "month": this.currentMonth + 1, "day": this.currentDay };
     //this.endDate={"year":this.currentYear,"month":this.currentMonth+1, "day":this.maxday};
     // this.tripEndDate={"year":this.currentYear,"month":this.currentMonth+1,"day":this.currentDay};
-
     this.tripEndDate = this.rideForm.value.startdate;
-    
   }
-
   create() {
-    this.rideForm.value.creatorName=this.creatorName1;
+    this.rideForm.value.creatorName = this.creatorName1;
     this.rideService.saveRide(this.rideForm.value)
       .subscribe((res: any) => {
         // console.log(res);
         // console.log("in ride create and create++++"+this.email)
-        console.log("+++++" +this.rideForm.value.startdate);
+        console.log("+++++" + this.rideForm.value.startdate);
         console.log(this.minDate + "%%%%%%%%")
         this.router.navigate(['/afterlogin2/riderhome3/' + this.email + '/rhomeimages/' + this.email])
       })
-
   }
-  findRiderByEmail(){
+  findRiderByEmail() {
     this.riderService.findRider2(this.email)
-    .subscribe((res: any) => {
-      console.log(res);
-      this.rider = res;
-      this.creatorName1=res.name;
-      console.log(this.rider.name);
-      console.log(this.creatorName1);
+      .subscribe((res: any) => {
+        console.log(res);
+        this.rider = res;
+        this.creatorName1 = res.name;
+        console.log(this.rider.name);
+        console.log(this.creatorName1);
       })
-    }
-   
- 
+  }
 }
