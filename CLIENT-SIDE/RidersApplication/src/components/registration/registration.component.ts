589def7024c34a47469d1e3ff2e31411f6455a84
Vagbhi Sharma <vagbhi@gmail.com>
2021-04-27 15:46:55 +0530

    Registration Validation

--- CLIENT-SIDE/RidersApplication/src/components/registration/registration.component.ts
+++ CLIENT-SIDE/RidersApplication/src/components/registration/registration.component.ts
@@ -21,21 +21,21 @@ export class RegistrationComponent implements OnInit {
       email: new FormControl("", [Validators.required,
       Validators.pattern("^(?! )[A-Za-z0-9._-]+@[a-z0-9.]+\.[a-z]{2,6}$")]),
 
-      ///^[ A-Za-z0-9_@./#&+-]*$/
+     
 
-      password: new FormControl("", [Validators.required, Validators.minLength(8), Validators.pattern("^(?! )[ A-Za-z0-9_@./#&+-]*$")]),
+      password: new FormControl("", [Validators.required, Validators.minLength(8), Validators.pattern("^(?! )[A-Za-z0-9_@./#&+-]*$")]),
       // password: new FormControl("", [
       //   Validators.required,
       //   Validators.pattern("(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$")
       // ]),
 
-      name: new FormControl("", [Validators.required, Validators.pattern("^(?! )[a-zA-Z ]{2,30}$")]),
+      name: new FormControl("", [Validators.required, Validators.pattern("^(?! )[a-zA-Z_ ]{2,30}$")]),
       phoneNumber: new FormControl("", [Validators.required, Validators.pattern("^(?! )[0-9]{10,14}$")]),
       gender: new FormControl(""),
       vehicleType: new FormControl(""),
-      vehicleNumber: new FormControl("", Validators.pattern("^(?! )[a-zA-Z ]{2,30}$")),
-      vehicleModel: new FormControl("", Validators.pattern("^(?! )[a-zA-Z ]{2,30}$")),
-      address: new FormControl("", Validators.pattern("^(?! )[a-zA-Z ]{2,30}$"))
+      vehicleNumber: new FormControl("", Validators.pattern("^(?! )[a-zA-Z0-9 _@./#,&+-]*$")),
+      vehicleModel: new FormControl("", Validators.pattern("^(?! )[a-zA-Z0-9 _@./#,&+-]*$")),
+      address: new FormControl("", Validators.pattern("^(?! )[a-zA-Z0-9 _@./#,&+-]*$"))
     });
   }
   getRegister() {
